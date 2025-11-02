ec33b403eb90e5b015cde3a68e1fd9f74613a3a0
Harshad Ade <harshadade143@gmail.com>
2025-11-02 13:26:50 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Gen AI Project',
+    version= '0.0.0',
+    author= 'Harshad Ade',
+    author_email= 'harshadade143@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
